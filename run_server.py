4503047da1541ca8dad8bab522a6df4a389c5977
unknown <anhthuxitin@outlook.com>
2025-12-06 22:06:41 +0700

    hotfix - watchfile

--- run_server.py
+++ run_server.py
@@ -1,11 +1,16 @@
 """Script để chạy FastAPI server."""
-
+import os
 import uvicorn
 
 if __name__ == "__main__":
+{
+    port = int(os.environ.get("PORT", 8000))
+
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8002,  # Port 8002 để tránh conflict với ai-be (port 8000)
-        reload=True,  # Auto-reload khi code thay đổi
+        port=port,
+        reload=False,
+        workers=1,
     )
+}
